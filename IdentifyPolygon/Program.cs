2336f691b251f08fc2c2f1805260d58d1d58029e
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-17 14:46:12 -0300

    feat(Program):creates IdentifyPolygon method

--- IdentifyPolygon/Program.cs
+++ IdentifyPolygon/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+    public static string IdentifyPolygon(int sideCount)
+    {
+        var name = string.Empty;
+
+        switch (sideCount)
+        {
+            case < 3:
+                name = "Não é um polígono";
+                break;
+            case 3:
+                name = "Triângulo";
+                break;
+            case 4:
+                name = "Quadrado";
+                break;
+            case 5:
+                name = "Pentágono";
+                break;
+            default:
+                name = "Polígono não identificado";
+                break;
+        }
+        return name;
+    }
