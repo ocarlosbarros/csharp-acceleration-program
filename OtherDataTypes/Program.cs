f7dc5f0b715e2192ce1d2e6f3c9f16f9ef152c44
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-16 21:56:19 -0300

    feat(Program):use enum example and show variables with implict type

--- OtherDataTypes/Program.cs
+++ OtherDataTypes/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OtherDataTypes;
+
+class Program
+{
+    public static void Main()
+    {
+        CardinalPointsEnum direction = CardinalPointsEnum.North;
+        Console.WriteLine($"Ponto Cardeal: {direction}");
+
+        //constants
+        const string URL = "https://www.betrybe.com/";
+        const int NUMBER_OF_ATTEMPTS_ALLOWED = 5;
+
+        //variables with implict type
+        var school = "Trybe";
+        var semester = 1;
+        var student = new { name = "Bruce Wayne", age = 22 };
+
+        Console.WriteLine($"Olá! Me chamo {student.name} sou um estudante da escola {school} estou no {semester} º semestre e tenho {student.age} anos");
+    }
+}
