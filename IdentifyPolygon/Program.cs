04d18e43263c398eb6059911150c7d8b9d9d2966
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-17 14:47:26 -0300

    feat(Program):creates Main method implementing IdentifyPolygon

--- IdentifyPolygon/Program.cs
+++ IdentifyPolygon/Program.cs
@@ -1,3 +1,20 @@
+public class Program
+{
+    public static void Main(){
+
+        Console.WriteLine("Informe um inteiro com o número de lados do polígono");
+        string? userEntry = Console.ReadLine();
+        bool validResponse = Int32.TryParse(userEntry, out var sideCount);
+
+        if (validResponse)
+        {
+            string response = IdentifyPolygon(sideCount);
+            Console.WriteLine(response);
+        }
+        
+        
+    }
+
     public static string IdentifyPolygon(int sideCount)
     {
         var name = string.Empty;
@@ -21,4 +38,5 @@
                 break;
         }
         return name;
+    }
     }
