948068ca940f6f76c4a12db61c6017d685ff1bf9
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-17 17:09:27 -0300

    feat(WhileAndDoWhile):creates project with use examples

---
+++ WhileAndDoWhile/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        UseWhile();
+        Console.WriteLine("=======================================");
+        UseDoWhile();
+        
+    }
+
+    public static void UseWhile()
+    {
+        int count = 0;
+        while (count < 10){
+            Console.WriteLine($"count {count}");
+            count++;
+        }
+    }
+
+    public static void UseDoWhile()
+    {
+        int count = 0;
+        do
+        {
+            Console.WriteLine("count " + count);
+            count++;
+        }
+        while (count < 10);
+    }
+}
