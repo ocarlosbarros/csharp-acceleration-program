8a6bb12a1f67fea74708754a9ec19c6ee2a951e9
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-17 13:50:08 -0300

    feat(Program):solution of the challenge

--- Installment/Program.cs
+++ Installment/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  
+    1- Mostrar o valor total (fixo)
+    2- Pedir para digital o número de parcelas
+    3- Informar o valor da parcela
+    */
+
+const double TOTAL_VALUE = 152.60;
+Console.WriteLine($"O valor total é de: R$ {TOTAL_VALUE}");
+Console.WriteLine("Informe o número de parcelas");
+string? installments = Console.ReadLine();
+int installmentsConverted = Convert.ToInt32(installments);
+Console.WriteLine($"Você informou {installmentsConverted} parcela(s)");
+double installmentAmount = TOTAL_VALUE / installmentsConverted;
+Console.WriteLine($"O valor da parcela é de: R$ {installmentAmount.ToString("N2")}");
