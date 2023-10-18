4066688756beca1fcc2e8325fed091a2d4189bb6
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-18 09:54:34 -0300

    feat(Program):loop example

--- AccessControll/Program.cs
+++ AccessControll/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        int numberOfPeople = 0;
+        int numberOfChildren = 0;
+        int age = 0;
+        
+        do
+        {
+            Console.WriteLine("Informe a idade da pessoa. Informe 0 para sair");
+            age = Convert.ToInt32(Console.ReadLine());
+            
+            if  (age == 0) break;
+            
+            numberOfPeople++;
+
+            if  (age < 18)  numberOfChildren++;
+
+        }while(age > 0);
+       
+        double rateOfChildren = 0;
+
+        if (numberOfPeople > 0) rateOfChildren = 100 * numberOfChildren / numberOfPeople;
+        
+        Console.WriteLine($"Existe {rateOfChildren} % de crianças");
+    }
+}
