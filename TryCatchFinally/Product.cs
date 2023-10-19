cb0d32b1301689ee24aede79dab67e082be5d23d
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-19 17:07:28 -0300

    feat(TryCatchFinaly):creates project

---
+++ TryCatchFinally/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    public class Product
+    {
+        public int ProductId {  get; set; }
+        public string? Name {  get; set; }
+        public bool IsValid {   get; set;  }
+    
+    }
+}
