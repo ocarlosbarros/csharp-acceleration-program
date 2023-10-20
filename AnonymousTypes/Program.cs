1b5b6265242f45464c73a326409de203ca9fdb8c
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-20 16:47:15 -0300

    feat(Program):adds anonymous types example

--- AnonymousTypes/Program.cs
+++ AnonymousTypes/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AnonymousTypes
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            /*  Anonymous types
+                Is a set of read-only properties into a single object without define a type first
+                */
+            var myAnonymousType = new { Amount = 42, Message = "Olá",  Value = 3.95};
+            Console.WriteLine(myAnonymousType.Amount);
+            Console.WriteLine(myAnonymousType.Message);
+            Console.WriteLine(myAnonymousType.Value);       
+        }
+    }
+}
