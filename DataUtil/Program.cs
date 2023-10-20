8aa944259a1b9acd0e1915b181fd32fa01e9f7f5
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-20 14:43:25 -0300

    refactor:rename project

---
+++ DataUtil/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DataUtil
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var date = new DateTime(2022, 10, 2, 8, 35, 0);
+            Console.WriteLine(date.ToString());
+        }
+    }
+
+}
