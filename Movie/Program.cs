831554f2d75b22decbff28f437a4761c2cb93eba
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-02 11:21:43 -0300

    feat:implements examples with Movie class

--- Movie/Program.cs
+++ Movie/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        var movie = new Movie("Matrix", "Ficção Científica");
+        var secondMovie = new Movie("Senhor dos Anéis: A sociedade do anel", "Fantasia");
+
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(secondMovie.Category);
+
+        movie.Title = "Matrix Revolution";
+
+        Console.WriteLine(movie.Title);
+
+        //movie.Category = "Action";//Unhandled exception. System.Exception: Category not allowed
+        movie.Category = "Fantasia";
+        Console.WriteLine(movie.Category);
+    }
+}
