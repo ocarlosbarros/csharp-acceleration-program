041ee627b1e2137677affcac804bd2f3b9594f10
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-02 14:20:26 -0300

    feat:creates example using static class and static members

---
+++ TrybeApp/Trybe.cs
@@ -0,0 +1,25 @@
+public static class Trybe
+{
+    public static string[] students = {"Luiza", "Bruno", "Patrícia", "Tiago"};
+
+    public static string[] Students
+    {
+        get { return students; }
+        set { students = value; }
+    }
+
+    public static int NumberOfStudents
+    {
+        get { return students.Length; }
+    }
+
+    public static bool IsStudent(string name)
+    {
+        return Array.Exists(students, student => student == name);
+    }
+
+    public static void PrintStudents()
+    {
+        Console.WriteLine(string.Join(", ", students));
+    }
+}
