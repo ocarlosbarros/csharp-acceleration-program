82831d3405a800a67395e92e3ff570b329b1a699
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-02 12:25:37 -0300

    feat(Rocket):changes get and set

--- RocketFactory/Rocket.cs
+++ RocketFactory/Rocket.cs
@@ -3,8 +3,21 @@
 public class Rocket
 {
     public string? Name {get; set;}
-    int Fuel {get; set;} = 0;
+    int _fuel {get; set;} = 0;
+    int _maxFuel = 5000;
     public decimal Cost {get;set;}
+
+    public int FuelAsPercentage
+    {
+        get { return _fuel * 100 / _maxFuel;}
+        set
+        {
+            if (value > 100 || value < 0)
+                throw new ArgumentOutOfRangeException();
+            
+            _fuel = (value * _maxFuel) / 100;
+        }
+    }
 
     public Rocket(string name, decimal cost)
     {
