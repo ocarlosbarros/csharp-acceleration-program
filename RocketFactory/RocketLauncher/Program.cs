c146eece277fab3546d21bb03f8b6829a0674850
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-02 10:31:25 -0300

    feat(RocketLauncher):creates a console app to implements class library project

---
+++ RocketFactory/RocketLauncher/Program.cs
@@ -0,0 +1,18 @@
+﻿using RocketFactory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var rocket = new Rocket("Apollo 11", 355000000m);
+        var secondRocket = new Rocket("Falcon 9", 90000000m);
+
+
+        Console.WriteLine(rocket.Name);
+        Console.WriteLine(secondRocket.Name);
+        Console.WriteLine(rocket.Cost);
+
+
+    }
+
+}
