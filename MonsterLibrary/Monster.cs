7df08f8161870a7eed1876b69ed633510ca60e55
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-16 17:13:40 -0300

    feat(Monster):creates a generic class Monster

---
+++ MonsterLibrary/Monster.cs
@@ -0,0 +1,18 @@
+﻿namespace MonsterLibrary;
+
+public class Monster
+{
+    public string Name {get;set;}
+    public Armor Armor {get;set;}
+
+    public void Attack()
+    {
+        Console.WriteLine($"{Name} fez um ataque");
+    }
+
+    public void Move(string location)
+    {
+        Console.WriteLine($"{Name} mudou-se para {location}");
+    }
+
+}
