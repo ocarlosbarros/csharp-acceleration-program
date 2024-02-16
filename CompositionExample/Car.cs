7b0f70aa2a8d9415c95b99246dbb4c28142a0b30
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-16 16:21:26 -0300

    feat(Car):creates a Car class with properties and methods

---
+++ CompositionExample/Car.cs
@@ -0,0 +1,41 @@
+public class Car
+{
+    public double TopSpeed {get;set;}
+    public bool IsAutomatic {get;set;}
+    public int NumbersOfSeats {get;set;}
+
+    public int EngineHorsePower {get;set;}
+    public int EngineTorque {get;set;}
+    public int EngineCapacity {get;set;}
+    public bool IsEngineStarted {get;set;}
+
+
+    public void Drive(double distanceKm, double speed )
+    {
+        if (speed > TopSpeed)
+            Console.WriteLine("Seu carro não pode ir tão rápido!");
+        else if (!IsEngineStarted)
+            Console.WriteLine("Seu carro não está ligado!");
+        else
+        {
+            var time = distanceKm / speed;
+            Console.WriteLine($"Você chegou em {time} horas.");
+        }
+    }
+
+    public void StartEngine()
+    {
+        if (IsEngineStarted)
+            Console.WriteLine("O motor já está ligado!");
+        else
+            IsEngineStarted = true;
+    }
+
+    public void StopEngine()
+    {
+        if (!IsEngineStarted)
+            Console.WriteLine("O motor já está parado!");
+        else
+            IsEngineStarted = false;
+    }
+}
