0ae74bd6ef5e3be92682cc01909a516391424145
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-16 17:24:50 -0300

    creates(Armor):creates a class Armor to use composition

--- MonsterLibrary/Armor.cs
+++ MonsterLibrary/Armor.cs
@@ -3,5 +3,5 @@ public class Armor
     public string Name {get;set;}
     public string Material {get;set;}
     public int Defense {get;set;}
-    public decimal Price{get;set}
+    public decimal Price{get;set;}
 }
