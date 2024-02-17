b8599f10b16c6f2de3b70e2c25d04d782517a518
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-17 16:03:40 -0300

    feat(InterfaceWithDefaultImplementation):creates a simple example

---
+++ InterfaceWithDefaultImplementation/Engine.cs
@@ -0,0 +1,9 @@
+public class Engine : IStartable
+{
+
+    public void Stop()
+    {
+        Console.WriteLine("Engine stoped!");
+    }   
+
+}
---
+++ InterfaceWithDefaultImplementation/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program 
+{
+    public static void Main(string[] args)
+    {
+        Engine engine = new Engine();
+        IStartable defaultImplementation = new Engine();
+
+        engine.Stop();
+        defaultImplementation.Start();
+        defaultImplementation.Stop();
+
+    }
+}
