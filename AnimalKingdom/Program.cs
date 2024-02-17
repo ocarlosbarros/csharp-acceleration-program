01554674ced2f3c2786bce253034122df2035bf9
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-17 16:47:26 -0300

    feat(Program):implements polymorphism in AnimalInfo

--- AnimalKingdom/Program.cs
+++ AnimalKingdom/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string [] args)
+    {
+        var wolf = new Wolf();
+        var cow = new Cow();
+
+        AnimalInfo(wolf);
+        AnimalInfo(cow);
+    }
+
+    public static void AnimalInfo(Animal animal)
+    {
+        Console.WriteLine($"{animal.GetType().Name} também conhecido como {animal.ScientificName}");
+        animal.Eat();
+    }
+}
