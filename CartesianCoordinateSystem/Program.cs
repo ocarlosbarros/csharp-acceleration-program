9dde926be123da1c6109cae55f69be9f7ba2fff8
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-20 13:43:39 -0300

    feat(Program):implements the program to use Point class

--- CartesianCoordinateSystem/Program.cs
+++ CartesianCoordinateSystem/Program.cs
@@ -2,6 +2,10 @@
 {
     static void Main()
     {
-        
+        var coordinate = new Point();
+        coordinate.x =  10;
+        coordinate.y = 20;
+
+        coordinate.PrintPoint(); 
     }
 }
