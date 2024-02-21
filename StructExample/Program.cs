52ac15c4cba454e39bad4cee0397c07f417b769d
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-21 15:30:40 -0300

    feat:Struct example

---
+++ StructExample/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Coordinate someLocation = new Coordinate(-19.9222072, -43.9339879);
+        Coordinate anotherCoordinate = new Coordinate();
+    }
+}
