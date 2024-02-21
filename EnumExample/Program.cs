13e9f875ad861d5c9e82fcdc34abdc1f362eddc0
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-21 15:45:31 -0300

    feat(Program):implements the program

--- EnumExample/Program.cs
+++ EnumExample/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Order order = new Order();
+        order.PayType = PaymentType.Credit;
+
+        Console.WriteLine("Crédito " + (int) PaymentType.Credit);
+        Console.WriteLine("Débito " + (int) PaymentType.Debit);
+        Console.WriteLine("Order " + order.PayType);
+    }
+}
