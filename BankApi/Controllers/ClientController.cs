9e9a4d6168eaeefe57891f51eba91ea83c3ac963
Carlos Barros <carlos_dbs@hotmail.com>
2024-03-02 17:43:14 -0300

    feat(ClientController):creates the get method

--- BankApi/Controllers/ClientController.cs
+++ BankApi/Controllers/ClientController.cs
@@ -4,6 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 [Route("clients")]
 public class ClientController : ControllerBase
 {
+    private static List<Client> _clients = new();
+    private static int _nextId = 1;
+
+    [HttpGet("{id}")]
+    public ActionResult Get(int id)
+    {
+        var client = _clients.FirstOrDefault(client => client.Id == id);
+
+        if (client == null)
+        {
+            return NotFound("Client not found");
+        }
+        return StatusCode(200, client);
+    }
+
     [HttpPost]
     public ActionResult Create(ClientRequest request)
     {
