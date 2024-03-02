59fe73378aa0e8ff757f386640f3e9da955d4e41
Carlos Barros <carlos_dbs@hotmail.com>
2024-03-02 17:40:57 -0300

    feat(ClientRequest): creates class with properties and createClient method

---
+++ BankApi/Server/ClientRequest.cs
@@ -0,0 +1,16 @@
+public class ClientRequest
+{
+    public string? Name {get;set;}
+    public decimal AccountBalance {get;set;}
+    
+    public Client CreateClient(int id)
+    {
+        return new Client
+        {
+            Id = id,
+            Name = Name,
+            AccountBalance = AccountBalance,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+    }
